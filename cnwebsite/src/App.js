f5e2275a0c8ea6f9e34422d9ac23e8ea6c1a472d
Cassius Naylor <cassius.cr.naylor@gmail.com>
2023-04-13 15:56:21 +0100

    Moved fonts folder to debug, built page structure and added images

--- cnwebsite/src/App.js
+++ cnwebsite/src/App.js
@@ -9,9 +9,8 @@ function App() {
         <div className="hero-content">
           <h1>Cassius Naylor</h1>
           <p className="hero-job">
-            <span>[SUBHEADER]</span>
+            <span>SOFTWARE DEVELOPER & D.E.I. CONSULTANT</span>
           </p>
-          <p className="hero-job-desc">[SUBTEXT]</p>
         </div>
 
         <div className="hero-arrow page-scroll home-arrow-down">
@@ -31,7 +30,7 @@ function App() {
         </div>
       </div>
 
-      <header id="masthead" className="site-header">
+      <header id="masthead" className="site-header" class="sticky-top">
         <nav
           id="primary-navigation"
           className="site-navigation"
@@ -51,8 +50,8 @@ function App() {
                 <span className="icon-bar"></span>
               </button>
 
-              <div className="page-scroll site-logo">
-                <a href="#top">Cass</a>
+              <div className="page-scroll site-logo sticky-top">
+                <a href="#top">Cassius Naylor</a>
               </div>
 
               <div
@@ -61,13 +60,16 @@ function App() {
               >
                 <ul className="nav navbar-nav navbar-right">
                   <li className="page-scroll">
-                    <a href="#top">About me</a>
+                    <a href="#intro">Main page</a>
                   </li>
                   <li className="page-scroll">
-                    <a href="#history">History</a>
+                    <a href="#aboutme">About me</a>
                   </li>
                   <li className="page-scroll">
-                    <a href="#works">Work</a>
+                    <a href="#tech">Technology</a>
+                  </li>
+                  <li className="page-scroll">
+                    <a href="#dei">Diversity, equity & inclusion</a>
                   </li>
                   <li className="page-scroll">
                     <a href="#contact">Contact</a>
@@ -83,9 +85,9 @@ function App() {
 
       <section class="site-section section-hello" id="intro">
         <div class="container">
-          <h2>HELLO &amp; WELCOME</h2>
+          <h2>WELCOME</h2>
           <p class="section-subtitle">
-            <span>EXPLORE OUR WORLD</span>
+            <span>TO MY CORNER OF THE INTERNET</span>
           </p>
           <div class="row">
             <div class="col-sm-4">
@@ -93,26 +95,20 @@ function App() {
                 <div class="rectangle">
                   <i class="fa fa-calendar" ariaHidden="true"></i>
                 </div>
-                <h3>We Meet Deadlines</h3>
-                <p>
-                  Your deadline is the most important value for us. Our job is
-                  to make sure the job is done perfectly before the deadline.
-                </p>
+                <h3>Building software</h3>
+                <p>[INSERT STUFF HERE ABOUT CODING AND TECH]</p>
               </div>
               <div class="main-service text-right">
                 <div class="rectangle">
                   <i class="fa fa-comments" ariaHidden="true"></i>
                 </div>
-                <h3>Communication Is Key</h3>
-                <p>
-                  Our team is available to check on your questions and take in
-                  feedback as soon as possibly.
-                </p>
+                <h3>My career so far</h3>
+                <p>[INSERT STUFF HERE ABOUT FT AND OTHER JOBS]</p>
               </div>
             </div>
             <div class="col-sm-4">
               <div class="big-rectangle">
-                <img src="assets/img/john-logo.png" alt=""></img>
+                <img src="assets/img/cn-images/logo2sq.jpg" alt=""></img>
               </div>
             </div>
             <div class="col-sm-4">
@@ -120,22 +116,15 @@ function App() {
                 <div class="rectangle">
                   <i class="fa fa-check" ariaHidden="true"></i>
                 </div>
-                <h3>Quality Control</h3>
-                <p>
-                  We test out everything to make sure that your project is fully
-                  functional, cross-browser compatible and meets your
-                  specifications.
-                </p>
+                <h3>Advocating for inclusion</h3>
+                <p>[STUFF HERE ABOUT MY WORK IN PROUD FT]</p>
               </div>
               <div class="main-service text-left">
                 <div class="rectangle">
                   <i class="fa fa-list" ariaHidden="true"></i>
                 </div>
-                <h3>Pixel Perfect</h3>
-                <p>
-                  We develop meaningful digital products and experiences that
-                  matter with design thinking and creative craftsmanship.
-                </p>
+                <h3>Writer, speaker, consultant</h3>
+                <p>[STUFF HERE ABOUT MY VOLUNTARY WORK AND PUBLISHED WORK]</p>
               </div>
             </div>
           </div>
@@ -144,11 +133,166 @@ function App() {
 
       <section class="section-background section-quote background-overlay text-center">
         <div class="container">
-          <p>
-            We make our <span>work look good</span> &amp; have experience in the
-            creation of brand identities for <span>print &amp; web</span>, with
-            a difference.
+          <p></p>
+        </div>
+      </section>
+
+      <section class="site-section section-hello" id="tech">
+        <div class="container">
+          <h2>TECHNOLOGY</h2>
+          <p class="section-subtitle">
+            <span>Some example projects</span>
           </p>
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="main-service text-right">
+                <div class="rectangle">
+                  <i class="fa fa-calendar" ariaHidden="true"></i>
+                </div>
+                <h3>Building software</h3>
+                <p>[INSERT STUFF HERE ABOUT CODING AND TECH]</p>
+              </div>
+              <div class="main-service text-right">
+                <div class="rectangle">
+                  <i class="fa fa-comments" ariaHidden="true"></i>
+                </div>
+                <h3>My career so far</h3>
+                <p>[INSERT STUFF HERE ABOUT FT AND OTHER JOBS]</p>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="big-rectangle">
+                <img src="assets/img/cn-images/logo2sq.jpg" alt=""></img>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="main-service text-left">
+                <div class="rectangle">
+                  <i class="fa fa-check" ariaHidden="true"></i>
+                </div>
+                <h3>Advocating for inclusion</h3>
+                <p>[STUFF HERE ABOUT MY WORK IN PROUD FT]</p>
+              </div>
+              <div class="main-service text-left">
+                <div class="rectangle">
+                  <i class="fa fa-list" ariaHidden="true"></i>
+                </div>
+                <h3>Writer, speaker, consultant</h3>
+                <p>[STUFF HERE ABOUT MY VOLUNTARY WORK AND PUBLISHED WORK]</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-background section-quote background-overlay text-center">
+        <div class="container">
+          <p></p>
+        </div>
+      </section>
+
+      <section class="site-section section-hello" id="dei">
+        <div class="container">
+          <h2>DIVERSITY, EQUITY AND INCLUSION</h2>
+          <p class="section-subtitle">
+            <span>Highlights of my work</span>
+          </p>
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="main-service text-right">
+                <div class="rectangle">
+                  <i class="fa fa-calendar" ariaHidden="true"></i>
+                </div>
+                <h3>Building software</h3>
+                <p>[INSERT STUFF HERE ABOUT CODING AND TECH]</p>
+              </div>
+              <div class="main-service text-right">
+                <div class="rectangle">
+                  <i class="fa fa-comments" ariaHidden="true"></i>
+                </div>
+                <h3>My career so far</h3>
+                <p>[INSERT STUFF HERE ABOUT FT AND OTHER JOBS]</p>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="big-rectangle">
+                <img src="assets/img/cn-images/logo2sq.jpg" alt=""></img>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="main-service text-left">
+                <div class="rectangle">
+                  <i class="fa fa-check" ariaHidden="true"></i>
+                </div>
+                <h3>Advocating for inclusion</h3>
+                <p>[STUFF HERE ABOUT MY WORK IN PROUD FT]</p>
+              </div>
+              <div class="main-service text-left">
+                <div class="rectangle">
+                  <i class="fa fa-list" ariaHidden="true"></i>
+                </div>
+                <h3>Writer, speaker, consultant</h3>
+                <p>[STUFF HERE ABOUT MY VOLUNTARY WORK AND PUBLISHED WORK]</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-background section-quote background-overlay text-center">
+        <div class="container">
+          <p></p>
+        </div>
+      </section>
+
+      <section class="site-section section-hello" id="contact">
+        <div class="container">
+          <h2>GET IN TOUCH</h2>
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="main-service text-right">
+                <div class="rectangle">
+                  <i class="fa fa-calendar" ariaHidden="true"></i>
+                </div>
+                <h3>Building software</h3>
+                <p>[INSERT STUFF HERE ABOUT CODING AND TECH]</p>
+              </div>
+              <div class="main-service text-right">
+                <div class="rectangle">
+                  <i class="fa fa-comments" ariaHidden="true"></i>
+                </div>
+                <h3>My career so far</h3>
+                <p>[INSERT STUFF HERE ABOUT FT AND OTHER JOBS]</p>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="big-rectangle">
+                <img src="assets/img/cn-images/logo2sq.jpg" alt=""></img>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="main-service text-left">
+                <div class="rectangle">
+                  <i class="fa fa-check" ariaHidden="true"></i>
+                </div>
+                <h3>Advocating for inclusion</h3>
+                <p>[STUFF HERE ABOUT MY WORK IN PROUD FT]</p>
+              </div>
+              <div class="main-service text-left">
+                <div class="rectangle">
+                  <i class="fa fa-list" ariaHidden="true"></i>
+                </div>
+                <h3>Writer, speaker, consultant</h3>
+                <p>[STUFF HERE ABOUT MY VOLUNTARY WORK AND PUBLISHED WORK]</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-background section-quote background-overlay text-center">
+        <div class="container">
+          <p></p>
         </div>
       </section>
     </span>
